a7a77f0fd0d0c59ac48cc906abd77e700ec11604
ertewu <wzy@wzyPc.(none)>
2013-12-25 12:14:24 +0800

    提交新文件

---
+++ src/com/example/demo/cases/prepare/CaseForGravity.java
@@ -0,0 +1,17 @@
+package com.example.demo.cases.prepare;
+
+
+public class CaseForGravity {
+    private static CaseForGravity sCase;
+
+    public static CaseForGravity obtain() {
+        if (sCase == null) {
+            sCase = new CaseForGravity();
+        }
+        return sCase;
+    }
+
+    public void work() {
+
+    }
+}
---
+++ src/com/example/demo/cases/prepare/Case_ContentObserverVsDataSetObserver.java
@@ -0,0 +1,5 @@
+package com.example.demo.cases.prepare;
+
+public class Case_ContentObserverVsDataSetObserver {
+
+}
