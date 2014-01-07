4843c0e2f2b85684758d391a83d613b1be080bb4
ertewu <wzy@wzyPc.(none)>
2014-01-07 10:18:39 +0800

    增加IntrinsicWidth case

---
+++ src/com/example/demo/cases/prepare/CaseGetIntrinsicWidth.java
@@ -0,0 +1,10 @@
+package com.example.demo.cases.prepare;
+
+/**
+ * 起源于领航桌面的IosLikeButton中根据图片大小确定控件大小的代码，
+ * 里边全是用getIntrinsicWidth,getIntrinsicHeight，而且我发现这个和scale有关，所以我决定列为demo
+ *
+ */
+public class CaseGetIntrinsicWidth {
+
+}
