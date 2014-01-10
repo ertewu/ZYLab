3130a8e28052263fbcbf2e857f3eddf6e9141c4b
ertewu <ertewu@163.com>
2014-01-11 00:04:11 +0800

    完善LogUtil

--- src/com/util/LogUtils.java
+++ src/com/util/LogUtils.java
@@ -9,54 +9,46 @@ public class LogUtils {
     }
 
     /**
-     * 能打印出当前函数的invoke，继而看出问题来.. 这个是我自己写的..
-     *
-     * @param tag
-     */
-    public static void footPrint(String tag) {
-        String msgToPrint = Thread.currentThread().getId() + "|" + "."
-                + Thread.currentThread().getStackTrace()[4].getMethodName()
-                + "|"
-                + Thread.currentThread().getStackTrace()[5].getMethodName()
-                + "|"
-                + Thread.currentThread().getStackTrace()[6].getMethodName();
-        Log.i("ertewu", tag + ":" + msgToPrint);
-    }
-
-    /**
      * 这个footPrint是从项目中拿出来的，应该是最好用的
      */
-    public static void footPrint2() {
-        String className = Thread.currentThread().getStackTrace()[3]
-                .getClassName();
+    public static void footPrint(String appendMsg) {
+        String msgToPrint = "--------------footprint start------------------\n";
+        String className = Thread.currentThread().getStackTrace()[3].getClassName();
         int index = className.lastIndexOf(".");
         if (index > -1) {
             className = className.substring(index + 1);
         }
-        String msgToPrint = Thread.currentThread().getId() + " " + className
-                + "."
+        msgToPrint = Thread.currentThread().getId() + " " + className + "."
                 + Thread.currentThread().getStackTrace()[3].getMethodName();
+        msgToPrint = msgToPrint + "\n";
+        msgToPrint = msgToPrint + appendMsg + "\n";
+        msgToPrint = msgToPrint + "--------------footprint end----------------";
         Log.i("ertewu", msgToPrint);
     }
 
     /**
      * 这个是意思我不懂为什么是Thread.currentThread.getStackTrace()[3],那0,1,2是做什么的？所以我试一下
      */
-    public static void stackTraceDemo() {
+    public static void printStackTrace(String appendMsg) {
+        StringBuilder builder = new StringBuilder();
+        builder.append("--------------printStackTrace start----------------\n");
         StackTraceElement[] array = Thread.currentThread().getStackTrace();
         if (null != array) {
             for (int i = 0; i < array.length; i++) {
-                StackTraceElement item = array[i];
-                String methodName = item.getMethodName();
-                String className = item.getClassName();
-                // 因为className中有很多包名，看上去很不方便，所以我们只想让其显示最后一个点后边的东西，也就是类的名字
-                int classIndex = className.lastIndexOf(".");
-                className = className.substring(classIndex + 1);
-                String logStr = Thread.currentThread().getId() + " "
-                        + Thread.currentThread().getName() + " " + className
-                        + "  " + methodName;
-                log(logStr);
+                if (i > 3) {
+                    StackTraceElement item = array[i];
+                    String methodName = item.getMethodName();
+                    String className = item.getClassName();
+                    // 因为className中有很多包名，看上去很不方便，所以我们只想让其显示最后一个点后边的东西，也就是类的名字
+                    int classIndex = className.lastIndexOf(".");
+                    className = className.substring(classIndex + 1);
+                    String logStr = Thread.currentThread().getId() + " " + Thread.currentThread().getName() + " " + className
+                            + "  " + methodName + "\n";
+                    builder.append(logStr);
+                }
             }
+            builder.append(appendMsg + "\n");
+            builder.append("--------------printStackTrace end----------------");
         }
     }
 
