9151724ed032a1e9f9be9ef80cf36af12ec03ead
ertewu <wzy@wzyPc.(none)>
2014-02-21 19:08:43 +0800

    再增加java下的正则使用例子以及doc文档文件夹

---
+++ src/com/example/demo/cases/CaseForRegex.java
@@ -0,0 +1,69 @@
+package com.example.demo.cases;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import android.text.TextUtils;
+import android.util.Log;
+
+public class CaseForRegex {
+
+    static String URL_1 = "http://news.sohu.com/20140220/n395324798.shtml";
+    static String URL_2 = "http://www.sohu.com/20140220/n395324798.shtml";
+    static String URL_3 = "http://sohu.com/20140220/n395324798.shtml";
+    static String URL_4 = "file:///sohu.com/20140220/n395324798.shtml";
+    static String URL_5 = "http://sohu.com.cn/20140220/n395324798.shtml";
+    static String URL_6 = "http://asohu.comb.cn/20140220/n395324798.shtml";
+    static String URL_7 = "http://soHu.com.cn/20140220/n395324798.shtml";
+
+    public void work() {
+        logHostName(URL_1);
+        logHostName(URL_2);
+        logHostName(URL_3);
+        logHostName(URL_4);
+        logHostName(URL_5);
+        logHostName(URL_6);
+        logHostName(URL_7);
+    }
+
+    private void logHostName(String urlstr) {
+        try {
+            URL url = new URL(urlstr);
+            String host = url.getHost();
+
+            if (!TextUtils.isEmpty(host)) {
+                Log.i("ertewu", "host is:" + host);
+                boolean result = isSohu1(host);
+                // boolean result = isSohu2(host);
+                // boolean result = isSohu3(host);
+                Log.i("ertewu", "result is:" + result + "\n"
+                        + "------------------");
+            } else {
+                Log.i("ertewu", "host is null" + "\n" + "--------------");
+            }
+        } catch (MalformedURLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    private boolean isSohu1(String host) {
+        String regex = "((\\w+\\.)?)(sohu\\.com)((\\.\\w+)?)";
+        Pattern p = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
+        Matcher m = p.matcher(host);
+        boolean b = m.matches();
+        return b;
+    }
+
+    private boolean isSohu2(String host) {
+        String regex = "((.*\\.)|)sohu\\.com(|(\\..*))";
+        return host.matches(regex);
+    }
+
+    private boolean isSohu3(String host) {
+        String regex = "(([\\s\\S]*\\.)|)sohu\\.com(|(\\.[\\s\\S]*))";
+        return host.matches(regex);
+    }
+}
---
+++ src/com/example/demo/cases/prepare/CaseForViewRootImpl.java
@@ -0,0 +1,72 @@
+package com.example.demo.cases.prepare;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.util.LogUtils;
+
+/**
+ * 主要是通过log查看一个View 的onMeasure,onLayout,onDraw是从哪里调用的，即起源于ViewRootImpl的哪里
+ * 从log来看，确实是performTraversal中的顺序，onMeasure第一，onLayout 第二， onDraw 最后调用 <br>
+ * 但是 这个例子中 onMeasure 与onLayout 不止走了一次，最后onDraw才走 <br>
+ * 可以再好好分析一下这个树..
+ *
+ * 下边还有keyDown 以及touch 的分析、以及focus的流程，focus的order探究，也用类似<br>
+ * 同时，ViewRootImpl中的draw函数，也很有看头的样子<br>
+ *
+ */
+public class CaseForViewRootImpl {
+
+    private static CaseForViewRootImpl sCase;
+    private Activity mAct;
+
+    public static CaseForViewRootImpl getInstance(Activity activity) {
+        if (null == sCase) {
+            sCase = new CaseForViewRootImpl(activity);
+        }
+        return sCase;
+    }
+
+    private CaseForViewRootImpl(Activity activity) {
+        this.mAct = activity;
+    }
+
+    public void work() {
+        View view = new LogView(mAct);
+        view.setLayoutParams(new ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT));
+        // view.setBackgroundResource(R.drawable.motor);
+        mAct.setContentView(view);
+    }
+
+    public static class LogView extends View {
+
+        public LogView(Context context) {
+            super(context);
+        }
+
+        @Override
+        protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+            LogUtils.printFunctionStack();
+            super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        }
+
+        @Override
+        protected void onLayout(boolean changed, int left, int top, int right,
+                int bottom) {
+            LogUtils.printFunctionStack();
+            super.onLayout(changed, left, top, right, bottom);
+        }
+
+        @Override
+        protected void onDraw(Canvas canvas) {
+            LogUtils.printFunctionStack();
+            super.onDraw(canvas);
+        }
+
+    }
+}
