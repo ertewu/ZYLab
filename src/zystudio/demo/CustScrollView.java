9500da14d21b8efd0f3cc3b2a25b86b154b63189
= <=>
2014-08-21 19:20:46 +0800

    navigation分支第一次备份提交

---
+++ src/zystudio/demo/CustScrollView.java
@@ -0,0 +1,13 @@
+package zystudio.demo;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ScrollView;
+
+public class CustScrollView extends ScrollView {
+
+    public CustScrollView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+}
--- src/zystudio/demo/MainActivity.java
+++ src/zystudio/demo/MainActivity.java
@@ -2,13 +2,73 @@ package zystudio.demo;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends Activity {
+
+    private ListView mListView;
+    private MyListAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // CaseInvoke.invokeCase(this);
-        // MicroCaseHelper.displayNormalCase(this);
+        setContentView(R.layout.qq_navigation);
+        mListView = (ListView) findViewById(R.id.navigation);
+        mAdapter = new MyListAdapter();
+        mListView.setAdapter(mAdapter);
+    }
+
+    private class MyListAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return 4;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            NavigationItemView view = new NavigationItemView(MainActivity.this);
+            switch (position) {
+            case 0:
+                view.setTitle("Title 0");
+                view.setImageViewHeight(100);
+                view.setImageViewColor(android.graphics.Color.YELLOW);
+                break;
+
+            case 1:
+                view.setTitle("Title 1");
+                view.setImageViewHeight(150);
+                view.setImageViewColor(android.graphics.Color.BLUE);
+                break;
+
+            case 2:
+                view.setTitle("Title 2");
+                view.setImageViewHeight(1000);
+                view.setImageViewColor(android.graphics.Color.GREEN);
+                break;
+
+            case 3:
+                view.setTitle("Title 3");
+                view.setImageViewHeight(1300);
+                view.setImageViewColor(android.graphics.Color.RED);
+                break;
+
+            }
+            return view;
+        }
+
     }
 }
