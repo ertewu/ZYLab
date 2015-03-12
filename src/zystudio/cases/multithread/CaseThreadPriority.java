04f1a6b20552b1b835b6a72ec649b8d6d4de02d5
ertewu <ertewu@gmail.com>
2015-03-12 23:28:33 +0800

    剩余提交

--- src/zystudio/cases/multithread/CaseThreadPriority.java
+++ src/zystudio/cases/multithread/CaseThreadPriority.java
@@ -1,12 +1,13 @@
 package zystudio.cases.multithread;
 
+import zystudio.mylib.utils.LogUtil;
 import android.app.Activity;
 import android.os.Process;
 import android.util.Log;
 
 /**
  * 测试 android中设置 线程优先级的
- *
+ * 
  */
 public class CaseThreadPriority {
 
@@ -26,7 +27,7 @@ public class CaseThreadPriority {
     }
 
     public void work() {
-        testThreadLife();
+        testBackGroundThread();
     }
 
     /**
@@ -39,9 +40,7 @@ public class CaseThreadPriority {
             public void run() {
                 while (true) {
                     try {
-                        Log.i("ertewu",
-                                "Thread is living:"
-                                        + System.currentTimeMillis());
+                        Log.i("ertewu", "Thread is living:" + System.currentTimeMillis());
                         Thread.sleep(100);
                     } catch (InterruptedException e) {
                         // TODO Auto-generated catch block
@@ -70,6 +69,54 @@ public class CaseThreadPriority {
         b.start();
     }
 
+    public void testBackGroundThread() {
+
+        final CountTask mainThreadTask = new CountTask();
+        final CountTask bgThreadTask = new CountTask();
+        Thread ThreadA = new Thread("ThreadA") {
+
+            @Override
+            public void run() {
+                mainThreadTask.run();
+            }
+        };
+        // ThreadA.setPriority(Process.THREAD_PRIORITY_DEFAULT);
+        Thread ThreadC = new Thread("ThreadC") {
+            @Override
+            public void run() {
+                bgThreadTask.run();
+            }
+        };
+        ThreadC.setPriority(Process.THREAD_PRIORITY_BACKGROUND);
+        ThreadA.start();
+        ThreadC.start();
+    }
+
+    private static class CountTask implements Runnable {
+
+        static boolean sIsFinished;
+        static int sCount = 0;
+        final int TOTAL = 100000;
+        static Object sLock = new Object();
+
+        @Override
+        public void run() {
+            while (sCount < TOTAL) {
+                    if (sIsFinished) {
+                        break;
+                    }
+                    if (sCount % 10000 == 0) {
+                        LogUtil.log("CountTask " + Thread.currentThread().getName() + " |sCount is:" + sCount);
+                    }
+                    if (sCount == TOTAL - 1) {
+                        LogUtil.log("CountTask" + Thread.currentThread().getName() + " End");
+                    }
+                    sCount++;
+                }
+                sIsFinished = true;
+        }
+    }
+
     private static class MyThread extends Thread {
         private int mOSPriority = Process.THREAD_PRIORITY_DEFAULT;
         private int mLoopCount = 0;
@@ -90,20 +137,14 @@ public class CaseThreadPriority {
                 mLoopCount++;
                 Math.log(Math.random() * 1000); // calculation test
 
-                Log.i("ertewu",
-                        new StringBuilder().append(getName())
-                                .append(" os priority: ").append(mOSPriority)
-                                .append(" java priority: ")
-                                .append(getPriority()).append(" loop count: ")
-                                .append(mLoopCount).toString());
+                LogUtil.log(new StringBuilder().append(getName()).append(" os priority: ").append(mOSPriority)
+                        .append(" java priority: ").append(getPriority()).append(" loop count: ").append(mLoopCount)
+                        .toString());
             }
 
-            Log.i("ertewu",
-                    new StringBuilder().append(getName()).append(" exiting...")
-                            .append(" os priority: ").append(mOSPriority)
-                            .append(" java priority: ").append(getPriority())
-                            .append(" loop count: ").append(mLoopCount)
-                            .toString());
+            LogUtil.log(new StringBuilder().append(getName()).append(" exiting...").append(" os priority: ")
+                    .append(mOSPriority).append(" java priority: ").append(getPriority()).append(" loop count: ")
+                    .append(mLoopCount).toString());
         }
     }
 }
