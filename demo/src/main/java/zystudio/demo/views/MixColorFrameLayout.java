48e95c975bad84928044cdaaa743510c840af010
ertewu <ertewu@163.com>
2017-03-22 19:22:23 +0800

    修改了一下MixColorFrameLayout，又多了一种文字画一半的实现方法

--- demo/src/main/java/zystudio/demo/views/MixColorFrameLayout.java
+++ demo/src/main/java/zystudio/demo/views/MixColorFrameLayout.java
@@ -14,8 +14,13 @@ public class MixColorFrameLayout extends FrameLayout {
 
     private PorterDuffXfermode myPorterDuff;
     private Paint mPaint;
-    final int mFirstColor=0xFFFFCC44;
-    final int mSecondColor=0xFF66AAFF;
+//    final int mFirstColor=0xFFFFCC44;
+//    final int mSecondColor=0xFF66AAFF;
+
+    //红色
+    final int mFirstColor= 0xFFFF0000;
+    //黄色
+    final int mSecondColor=0xFFFFFF00;
 
     public MixColorFrameLayout(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -25,6 +30,13 @@ public class MixColorFrameLayout extends FrameLayout {
 
     @Override
     protected void dispatchDraw(Canvas canvas) {
+//        dispatchDraw1(canvas);
+        dispatchDraw2(canvas);
+    }
+
+
+    private void dispatchDraw1(Canvas canvas){
+        //这是用xferMode的
         canvas.drawColor(Color.WHITE);
         mPaint.setFlags(Paint.ANTI_ALIAS_FLAG);
         mPaint.setTextSize(100);
@@ -36,7 +48,6 @@ public class MixColorFrameLayout extends FrameLayout {
 //        mPaint.setFlags(Paint.ANTI_ALIAS_FLAG);
 //        mPaint.setTextSize(100);
 //        mPaint.setColor(mSecondColor);
-        canvas.drawText("WZY NIHAO HELLO", 50, 200, mPaint);
 
         mPaint.setXfermode(myPorterDuff);
         mPaint.setStyle(Style.FILL);
@@ -44,6 +55,32 @@ public class MixColorFrameLayout extends FrameLayout {
         Rect rect=new Rect(50,50, 230, 250);
         canvas.drawRect(rect, mPaint);
         mPaint.setXfermode(null);
+        canvas.drawText("WZY NIHAO HELLO", 50, 200, mPaint);
+        canvas.restoreToCount(sc);
+    }
+
+   //这个是重画第二次文字的
+    private void dispatchDraw2(Canvas canvas){
+        canvas.drawColor(Color.WHITE);
+        mPaint.setFlags(Paint.ANTI_ALIAS_FLAG);
+        mPaint.setTextSize(100);
+        mPaint.setColor(mSecondColor);
+        canvas.drawText("WZY NIHAO HELLO", 50, 200, mPaint);
+        int sc = canvas.saveLayer(50, 50, 230,250,null,Canvas.MATRIX_SAVE_FLAG | Canvas.CLIP_SAVE_FLAG
+                | Canvas.HAS_ALPHA_LAYER_SAVE_FLAG | Canvas.FULL_COLOR_LAYER_SAVE_FLAG
+                | Canvas.CLIP_TO_LAYER_SAVE_FLAG);
+//        mPaint.setFlags(Paint.ANTI_ALIAS_FLAG);
+//        mPaint.setTextSize(100);
+//        mPaint.setColor(mSecondColor);
+
+//        mPaint.setXfermode(myPorterDuff);
+//        mPaint.setStyle(Style.FILL);
+        canvas.clipRect(50,50,110,250);
+        mPaint.setColor(mFirstColor);
+//        Rect rect=new Rect(50,50, 230, 250);
+//        canvas.drawRect(rect, mPaint);
+//        mPaint.setXfermode(null);
+        canvas.drawText("WZY NIHAO HELLO", 50, 200, mPaint);
         canvas.restoreToCount(sc);
     }
 }
