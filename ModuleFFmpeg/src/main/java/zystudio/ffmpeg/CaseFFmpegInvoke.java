26dae544f813fdc0683f782abe12072018b5b3db
user <user@MBPjiuyu.local>
2018-11-15 12:56:11 +0800

    http用ff_dump_stream_info已经可以使用了，https的还不行，基本上功能达到了，很好继续

--- ModuleFFmpeg/src/main/java/zystudio/ffmpeg/CaseFFmpegInvoke.java
+++ ModuleFFmpeg/src/main/java/zystudio/ffmpeg/CaseFFmpegInvoke.java
@@ -2,7 +2,15 @@ package zystudio.ffmpeg;
 
 public class CaseFFmpegInvoke {
 
-    private static final String URL="https://bizcommon.alicdn.com/KM596pxVraOsnEmgAP7/9RRTJjqkg6FyoQIDu1f%40%40hd.mp4";
+//    private static final String URL="https://bizcommon.alicdn.com/KM596pxVraOsnEmgAP7/9RRTJjqkg6FyoQIDu1f%40%40hd.mp4";
+
+    //http是可以的，上边的Https 就不太行，估计是没加ssl..
+    private static final String URL="http://bizcommon.alicdn.com/KM596pxVraOsnEmgAP7/9RRTJjqkg6FyoQIDu1f%40%40hd.mp4";
+
+
+    // 本地url 是可以的，上边那个就不太行
+//    private static final String URL="/sdcard/0_MyDir/vid.mp4";
+
     private FFmpegUtil mUtil;
 
     public CaseFFmpegInvoke(){
